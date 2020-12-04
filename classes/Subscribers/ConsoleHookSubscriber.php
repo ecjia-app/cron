6791dff51d8304a99171733e73733bdc3dc7f9b2
王正东 <wangzhengdong@ecmoban.com>
2020-12-04 17:35:55 +0800

    优化Hooker加载

--- classes/Subscribers/ConsoleHookSubscriber.php
+++ classes/Subscribers/ConsoleHookSubscriber.php
@@ -5,9 +5,7 @@ namespace Ecjia\App\Cron\Subscribers;
 
 
 use Ecjia\App\Cron\CronRun;
-use ecjia_admin;
 use RC_Cron;
-use RC_Uri;
 use Royalcms\Component\Hook\Dispatcher;
 
 class ConsoleHookSubscriber
@@ -22,6 +20,10 @@ class ConsoleHookSubscriber
      */
     public function subscribe(Dispatcher $events)
     {
+        if (!class_exists('RC_Cron')) {
+            return;
+        }
+
         // Log only error jobs to database
         RC_Cron::setLogOnlyErrorJobsToDatabase(true);
         // Add a cron job
